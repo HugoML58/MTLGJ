b36a7deeedef9f91e1826fd577ac3480865c2756
DragonGawain <craigk00@hotmail.com>
2023-04-28 22:39:50 -0400

    format things

--- MTLGJ/Assets/_Scripts/Interactions/PlayerInteract.cs
+++ MTLGJ/Assets/_Scripts/Interactions/PlayerInteract.cs
@@ -2,36 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerInteract : MonoBehaviour
 {
-    [SerializeField] InputsListener Input;
-    [SerializeField] LayerMask interactable;
+    [SerializeField]
+    InputsListener Input;
+
+    [SerializeField]
+    LayerMask interactable;
 
     private float interactRange = 2f;
 
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.Interact)
         {
-            
-            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange, interactable);
+            Collider[] colliderArray = Physics.OverlapSphere(
+                transform.position,
+                interactRange,
+                interactable
+            );
             for (int i = 0; i < colliderArray.Length; i++)
             {
                 if (colliderArray[i].TryGetComponent(out Interactable interactable))
-                { 
+                {
                     interactable.Interact();
                 }
-                Debug.Log(colliderArray[i].transform.gameObject.name);
             }
-
         }
     }
 
@@ -41,6 +41,4 @@ public class PlayerInteract : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, interactRange);
     }
-
-    
 }
