fb5733fcc9bbbc639920d2cb8fd4634834a4f0c1
DragonGawain <craigk00@hotmail.com>
2023-04-29 11:33:12 -0400

    Updated interact to ray

--- MTLGJ/Assets/_Scripts/Player/PlayerInteract.cs
+++ MTLGJ/Assets/_Scripts/Player/PlayerInteract.cs
@@ -8,19 +8,31 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField]
     float interactRange = 2f;
 
+    [SerializeField]
+    Transform Camera;
+
     void Update()
     {
         if (Input.Interact)
         {
-            Collider[] colliderArray = Physics.OverlapSphere(
-                transform.position,
+            bool collision = Physics.Raycast(
+                Camera.position,
+                Camera.forward,
+                out RaycastHit HitInfo,
                 interactRange
             );
-            for (int i = 0; i < colliderArray.Length; i++)
+
+            // if (collision.TryGetComponent(out Interactable interactable))
+            // {
+            //     interactable.Interact();
+            // }
+
+            if (collision)
             {
-                if (colliderArray[i].TryGetComponent(out Interactable interactable))
+                if (HitInfo.transform.TryGetComponent(out Interactable interactable))
                 {
                     interactable.Interact();
+                    Debug.Log("hit");
                 }
             }
         }
@@ -28,7 +40,11 @@ public class PlayerInteract : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, interactRange);
+        // Gizmos.color = Color.yellow;
+        // Gizmos.DrawWireSphere(transform.position, interactRange);
+
+        Gizmos.color = Color.blue;
+        Ray ray = new Ray(Camera.position, Camera.forward*interactRange);
+        Gizmos.DrawRay(ray);
     }
 }
